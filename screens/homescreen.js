250fe9904ac53ff0be2fd037ef18cfe2ae6e7242
asad44135 <56040899+asad44135@users.noreply.github.com>
2021-08-22 18:36:06 +0530

    Add files via upload

---
+++ screens/homescreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const homescreen = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default homescreen
+
+const styles = StyleSheet.create({})
